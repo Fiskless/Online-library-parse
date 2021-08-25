2061f70b7f0726a3003ed23ead8ba4b3248f60b1
Fiskless <Fiskless@gmail.com>
2021-08-26 03:14:34 +0500

    Download book with titles

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import requests
 import os
+from pathvalidate import sanitize_filename, sanitize_filepath
+from bs4 import BeautifulSoup
 
 
-def get_and_save_book(book_id):
-    response = requests.get(f"https://tululu.org/txt.php?id={book_id}")
+def get_book_title(book_id):
+    url_to_get_title = f"https://tululu.org/b{book_id}/"
+    response = requests.get(url_to_get_title)
     response.raise_for_status()
     check_for_redirect(response)
 
-    with open(f"books/id{book_id}.txt", "w") as file:
-        file.write(response.text)
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_title_and_author = soup.find('h1').text.split('::')
+    book_title = f'{book_id}. {book_title_and_author[0].strip()}'
+    return book_title
 
 
 def check_for_redirect(response):
@@ -16,11 +21,27 @@ def check_for_redirect(response):
         raise requests.exceptions.HTTPError
 
 
+def download_txt(url, filename, folder='books/'):
+    correct_filename = sanitize_filename(filename)
+    correct_folder = sanitize_filepath(folder)
+    path_to_book = os.path.join(correct_folder, f'{correct_filename}.txt')
+
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    with open(path_to_book, "w") as file:
+        file.write(response.text)
+    return path_to_book
+
+
 def main():
     os.makedirs("books", exist_ok=True)
     for book_id in range(1, 11):
         try:
-            get_and_save_book(book_id)
+            book_title = get_book_title(book_id)
+            url_to_download_book = f"https://tululu.org/txt.php?id={book_id}"
+            filepath = download_txt(url_to_download_book, book_title)
         except requests.exceptions.HTTPError:
             pass
 
