565e903504889d86042924a3453e29c7867e0434
Fiskless <Fiskless@gmail.com>
2021-08-26 00:12:46 +0500

    Handle the case when book doesn't exist

--- main.py
+++ main.py
@@ -5,14 +5,26 @@ import os
 def get_and_save_book(book_id):
     response = requests.get(f"https://tululu.org/txt.php?id={book_id}")
     response.raise_for_status()
+    check_for_redirect(response)
 
     with open(f"books/id{book_id}.txt", "w") as file:
         file.write(response.text)
 
 
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
+
+
+def main():
+    os.makedirs("books", exist_ok=True)
+    for book_id in range(1, 11):
+        try:
+            get_and_save_book(book_id)
+        except requests.exceptions.HTTPError:
+            pass
+
+
 if __name__ == '__main__':
 
-    os.makedirs("books", exist_ok=True)
-
-    for book_id in range(1, 11):
-        get_and_save_book(book_id)
+    main()
