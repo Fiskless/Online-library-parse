2d9e0e93316ce699158be9b26213ad364648acdd
Fiskless <Fiskless@gmail.com>
2021-08-26 18:55:24 +0500

    Make possibility to set book ids through the terminal

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import os
 from pathvalidate import sanitize_filename, sanitize_filepath
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlsplit
+import argparse
 
 
 def get_book_html(book_id):
-    url_to_get_title = f"https://tululu.org/b{book_id}/"
+    url_to_get_title = f'https://tululu.org/b{book_id}/'
     response = requests.get(url_to_get_title)
     response.raise_for_status()
     check_for_redirect(response)
@@ -14,12 +15,25 @@ def get_book_html(book_id):
     return response.text
 
 
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('start_id',
+                        help='Укажите номер книги, с которой начать скачивание',
+                        nargs='?',
+                        default=1)
+    parser.add_argument('end_id',
+                        help='Укажите номер книги, на которой закончить скачивание',
+                        nargs='?',
+                        default=10)
+
+    return parser
+
+
 def parse_book_page(html):
     soup = BeautifulSoup(html, 'lxml')
 
     book_title_and_author = soup.find('h1').text.split('::')
     book_title = book_title_and_author[0].strip()
-    # print(book_title)
 
     relative_picture_address = soup.find('div', class_='bookimage').find('img')['src']
     url_to_download_image = urljoin('https://tululu.org',
@@ -80,7 +94,12 @@ def download_image(url, filename, folder='images/'):
 def main():
     os.makedirs("books", exist_ok=True)
     os.makedirs("images", exist_ok=True)
-    for book_id in range(1, 11):
+    parser = create_parser()
+    args = parser.parse_args()
+    begin_with = int(args.start_id)
+    finish_on = int(args.end_id)
+
+    for book_id in range(begin_with, finish_on+1):
         try:
             book_page = parse_book_page(get_book_html(book_id))
             url_to_download_book = f"https://tululu.org/txt.php?id={book_id}"
